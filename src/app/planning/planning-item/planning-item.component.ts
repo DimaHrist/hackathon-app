d6585e5fa7bc3f043fc515877ae0fc9abec2c5eb
d.khristyukha <d.khristyukha@wilix.org>
2020-12-13 16:51:24 +0300

    Fixes

--- src/app/planning/planning-item/planning-item.component.ts
+++ src/app/planning/planning-item/planning-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -8,6 +8,8 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./planning-item.component.scss']
 })
 export class PlanningItemComponent implements OnInit {
+
+  @Input() list: any;
 
   public removeItem(): void {
 
